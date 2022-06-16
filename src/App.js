71a13aeae7d76b038a38b9191b4ecf027ba1882b
Adriano <adriano_alves_2008@hotmail.com>
2022-06-16 18:19:46 -0300

    add task

--- src/App.js
+++ src/App.js
@@ -2,30 +2,63 @@ import './global.css'
 import './styles.css'
 import { IoMdAdd } from 'react-icons/io'
 import { MdOutlineClose } from 'react-icons/md'
+import { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
+  const [task, setTask] = useState('')
+
+  const [tasks, setTasks] = useState([])
+
+  function handleCreateTask() {
+    if (task === '') {
+      // Se tiver vazio vai aparecer mensagem de erro
+      // Utilizei o reactTostify
+      toast.error('Digite alguma tarefa')
+    } else {
+      // Adicionar task
+      const idRandom = num => Math.floor(Math.random() * num)
+
+      // Coloquei um número aleatório grande no id, para não ter perigo de colisão de id
+      const newTask = { id: idRandom(1234565), title: task, isComplete: false }
+
+      setTasks([...tasks, newTask])
+    }
+  }
+
   return (
     <div className="App">
+      <ToastContainer />
       <div className="todo">
         <header>
-          <input type="text" />
-          <button>
+          {/* Para atualizar o valor do input, utilizamos o onChannge */}
+          {/* Vamos pegar o evento e ir atualizando */}
+          <input
+            type="text"
+            value={task}
+            onChange={ev => setTask(ev.target.value)}
+          />
+          <button onClick={handleCreateTask}>
             <IoMdAdd />
           </button>
         </header>
-        <div className="task-container">
-          <div className="check-and-title">
-            <label className="checkbox-container">
-              <input type="checkbox" />
 
-              <span className="checkmark"></span>
-            </label>
-            <p>Criar vídeo</p>
+        {tasks.map(task => (
+          <div key={task.id} className="task-container">
+            <div className="check-and-title">
+              <label className="checkbox-container">
+                <input type="checkbox" />
+
+                <span className="checkmark"></span>
+              </label>
+              <p>{task.title}</p>
+            </div>
+            <div>
+              <MdOutlineClose />
+            </div>
           </div>
-          <div>
-            <MdOutlineClose />
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   )
