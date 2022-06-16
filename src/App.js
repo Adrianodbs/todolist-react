df4178218865808b8af04ccc8565dd6ca6a2fc2c
Adriano <adriano_alves_2008@hotmail.com>
2022-06-16 13:13:21 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import './global.css'
+import './styles.css'
+import { IoMdAdd } from 'react-icons/io'
+
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <div className="todo">
+        <header>
+          <input type="text" />
+          <button>
+            <IoMdAdd />
+          </button>
+        </header>
+        <div className="task-container"></div>
+      </div>
+    </div>
+  )
 }
 
 export default App
