937c65342753c11f5e488ac482f3ec9497649344
Adriano <adriano_alves_2008@hotmail.com>
2022-06-16 13:36:19 -0300

    estilização

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './global.css'
 import './styles.css'
 import { IoMdAdd } from 'react-icons/io'
+import { MdOutlineClose } from 'react-icons/md'
 
 function App() {
   return (
@@ -12,7 +13,19 @@ function App() {
             <IoMdAdd />
           </button>
         </header>
-        <div className="task-container"></div>
+        <div className="task-container">
+          <div className="check-and-title">
+            <label className="checkbox-container">
+              <input type="checkbox" />
+
+              <span className="checkmark"></span>
+            </label>
+            <p>Criar vídeo</p>
+          </div>
+          <div>
+            <MdOutlineClose />
+          </div>
+        </div>
       </div>
     </div>
   )
